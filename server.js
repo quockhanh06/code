4cd3fc98568b6ec388a30e20984edb37a9b6f371
quockhanh06 <nanana1221224@gmail.com>
2026-04-08 15:19:46 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// dữ liệu giả (sau này sẽ là database)
+const products = ["Áo", "Quần", "Giày"];
+
+// API trả dữ liệu
+app.get("/api/products", (req, res) => {
+    res.json(products);
+});
+
+app.listen(3000, () => {
+    console.log("Server chạy tại http://localhost:3000");
+});
